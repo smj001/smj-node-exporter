191d69de2acd028ebd0d8ec9d418fbc4424cd3cb
Mohammad Javadi <s.m.javadi001@gmail.com>
2021-12-08 13:21:04 +0330

    add files

---
+++ node-exporter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export $(cat .env | xarg)
+mkdir /node-exporter
+cd /node-exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar -xzf node_exporter-1.3.1.linux-amd64.tar.gz
+
+
+cat << EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+
+Description=node_exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+
+User=root
+Type=simple
+ExecStart=/node-exporter/node_exporter-1.3.1.linux-amd64/node_exporter --web.listen-address=:[$EXPORTER_PORT]
+
+[Install]
+
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl status node_exporter 
