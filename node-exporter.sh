02fcda482537ed12c113dafb570df0eb1f2c6de4
smj001 <s.m.javadi001@gmail.com>
2021-12-08 18:15:37 +0330

    bug fix - xargs

--- node-exporter.sh
+++ node-exporter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export $(cat .env | xarg)
+export $(cat .env | xargs)
 mkdir /node-exporter
 cd /node-exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
